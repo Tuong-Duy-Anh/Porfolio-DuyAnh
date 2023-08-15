a62b70a2d0f3dc657a5acb0b1faeb4919665dc02
Tuong-Duy-Anh <tdanh.ump@gmail.com>
2023-08-15 23:37:19 +0700

    finish contact and start canvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./earth_hologram/scene.gltf");
+
+  return (
+    <primitive object={earth.scene} scale={1.5} position-y={0} rotation-y={0} />
+  );
 };
 
-export default Earth;
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 40,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
